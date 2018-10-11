136b64f50a5ffd169f7fc88ee93ef6f7eb24c41c
Drake Rentrop <drakerentrop@gmail.com>
2018-10-11 14:10:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 def take_a_number(katz_deli, name)
   katz_deli << name
-  "Welcome, #{name}. You are number #{katz_deli.length} in line."
+puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
-    "There is nobody waiting to be served!"
+  puts  "There is nobody waiting to be served!"
   else
-    "Currently serving #{katz_deli.shift}"
+  puts  "Currently serving #{katz_deli.shift}"
   end
 end
 def line(katz_deli)
